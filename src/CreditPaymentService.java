eaf0e633cb81c598fd13b914c2a81653910e3edf
Artyel <artyel@icloud.com>
2023-02-28 09:13:28 +0000

    Formula modified

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,9 +1,9 @@
 public class CreditPaymentService {
     public double calculate(double a, int b) {
-        double c = ((a * (Math.pow(1 + a, b))) / ((Math.pow(1 + a, b)) - 1));
+        double rate = ((a * (Math.pow(1 + a, b))) / ((Math.pow(1 + a, b)) - 1));
 
 
-        return c;
+        return rate;
 
 
     }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
 
         Double a = 9.99 / 12 / 100;
-        int b = 36;
+        int b = 12;
         int credit = 1_000_000;
         double rate = service.calculate(a, b);
         int payment = (int) (credit * rate);
